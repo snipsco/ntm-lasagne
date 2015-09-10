fe5d8c87f976d1d73f26f713919193da21a0ae84
Tristan Deleu <tristan.deleu@gmail.com>
2015-09-10 14:30:23 +0200

    Add Memory component, inheriting from InputLayer

---
+++ ntm/memory.py
@@ -0,0 +1,19 @@
+import theano
+import theano.tensor as T
+import numpy as np
+
+from lasagne.layers import InputLayer
+
+
+class Memory(InputLayer):
+    """
+    docstring for Memory
+    """
+    def __init__(self, shape, 
+        memory_init=lasagne.init.Constant(0.),
+        learn_init=True,
+        **kwargs):
+        super(Memory, self).__init__(shape, **kwargs)
+        self.memory_init = self.add_param(
+            memory_init, shape,
+            name=self.name, trainable=learn_init, regularizable=False)
