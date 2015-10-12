fef17579a8a084987ea5e413ad512662ab24aa56
Tristan Deleu <tristan.deleu@gmail.com>
2015-10-12 10:02:41 +0200

    Replace T.norm in the cosine similarity

--- ntm/similarities.py
+++ ntm/similarities.py
@@ -6,6 +6,6 @@ import numpy as np
 def cosine_similarity(x, y, eps=1e-9):
     y = y.dimshuffle(1, 0)
     z = T.dot(x, y)
-    z /= x.norm(2) * y.norm(2, axis=0).dimshuffle('x', 0) + eps
+    z /= T.sqrt(T.sum(x * x) * T.sum(y * y, axis=0).dimshuffle('x', 0) + 1e-6)
 
     return z
