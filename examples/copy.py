Fixed get_params if sign is None

examples/copy.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from lasagne.layers import InputLayer, DenseLayer, ReshapeLayer
 import lasagne.layers
@@ -48,22 +49,38 @@ pred = T.clip(lasagne.layers.get_output(l_out), 1e-10, 1. - 1e-10)
 loss = T.mean(lasagne.objectives.binary_crossentropy(pred, target))
 
 params = lasagne.layers.get_all_params(l_out, trainable=True)
-updates = graves_rmsprop(loss, params, beta=1e-3)
+updates = graves_rmsprop(loss, params, beta=1e-4)
 
 train_fn = theano.function([input_var, target], loss, updates=updates)
 ntm_fn = theano.function([input_var], pred)
 
-max_sequences = 500000
-max_length = 5
-scores = []
-for batch in range(max_sequences):
-    length = random.randint(1, max_length)
-    i, o = copy(8, length)
-    score = train_fn(i, o)
-    scores.append(score)
-    if batch % 500 == 0:
-        print 'Batch #%d: %.6f' % (batch, np.mean(score))
-        scores = []
+try:
+    max_sequences = 500000
+    max_length = 5
+    scores = []
+    all_scores = []
+    for batch in range(max_sequences):
+        length = random.randint(1, max_length)
+        i, o = copy(8, length)
+        score = train_fn(i, o)
+        scores.append(score)
+        all_scores.append(score)
+        if batch % 500 == 0:
+            print 'Batch #%d: %.6f' % (batch, np.mean(scores))
+            scores = []
+except KeyboardInterrupt:
+    pass
+
+def learning_curve():
+    sc = pd.Series(all_scores)
+    ma = pd.rolling_mean(sc, window=500)
+
+    ax = plt.subplot(1, 1, 1)
+    ax.plot(sc.index, sc, color='lightgray')
+    ax.plot(ma.index, ma, color='red')
+    ax.set_yscale('log')
+    ax.set_xlim(sc.index.min(), sc.index.max())
+    plt.show()
 
 def viz(n):
     i, o = copy(8, n)