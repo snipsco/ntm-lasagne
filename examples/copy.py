96636e3d1afcf17fd6557d12deb4b615480da110
Tristan Deleu <tristan.deleu@gmail.com>
2015-11-24 16:10:27 +0100

    Refactor visualization tools & task generators

--- examples/copy.py
+++ examples/copy.py
@@ -16,18 +16,19 @@ from ntm.ntm import NTMLayer
 from ntm.memory import Memory
 from ntm.controllers import DenseController
 from ntm.heads import WriteHead, ReadHead
-from ntm.tasks import copy
 from ntm.updates import graves_rmsprop
+
+from utils.generators import CopyTask
+from utils.visualization import Dashboard
 
 input_var = T.dtensor3('input')
 target = T.dtensor3('target')
 
-n = 20
+size = 8
 num_units = 100
-l = 8
 memory_shape = (128, 20)
 
-l_input = InputLayer((1, None, l + 1), input_var=input_var)
+l_input = InputLayer((1, None, size + 1), input_var=input_var)
 _, seqlen, _ = l_input.input_var.shape
 # Neural Turing Machine Layer
 memory = Memory(memory_shape, name='memory', learn_init=False)
@@ -44,9 +45,9 @@ heads = [
 l_ntm = NTMLayer(l_input, memory=memory, controller=controller, \
       heads=heads)
 l_shp = ReshapeLayer(l_ntm, (-1, num_units))
-l_dense = DenseLayer(l_shp, num_units=l + 1, nonlinearity=lasagne.nonlinearities.sigmoid, \
+l_dense = DenseLayer(l_shp, num_units=size + 1, nonlinearity=lasagne.nonlinearities.sigmoid, \
     name='dense')
-l_out = ReshapeLayer(l_dense, (1, seqlen, l + 1))
+l_out = ReshapeLayer(l_dense, (1, seqlen, size + 1))
 
 pred = T.clip(lasagne.layers.get_output(l_out), 1e-10, 1. - 1e-10)
 loss = T.mean(lasagne.objectives.binary_crossentropy(pred, target))
@@ -58,72 +59,25 @@ train_fn = theano.function([input_var, target], loss, updates=updates)
 ntm_fn = theano.function([input_var], pred)
 ntm_layer_fn = theano.function([input_var], lasagne.layers.get_output(l_ntm, get_details=True))
 
+generator = CopyTask(max_iter=500000, size=8, max_length=5)
+
 try:
-    max_sequences = 500000
-    max_length = 5
-    scores = []
-    all_scores = []
-    for batch in range(max_sequences):
-        length = random.randint(1, max_length)
-        i, o = copy(8, length)
-        score = train_fn(i, o)
+    scores, all_scores = [], []
+    for i, (example_input, example_output) in generator:
+        score = train_fn(example_input, example_output)
         scores.append(score)
         all_scores.append(score)
-        if batch % 500 == 0:
-            print 'Batch #%d: %.6f' % (batch, np.mean(scores))
+        if i % 500 == 0:
+            print 'Batch #%d: %.6f' % (i, np.mean(scores))
             scores = []
 except KeyboardInterrupt:
     pass
 
-def learning_curve():
-    sc = pd.Series(all_scores)
-    ma = pd.rolling_mean(sc, window=500)
-
-    ax = plt.subplot(1, 1, 1)
-    ax.plot(sc.index, sc, color='lightgray')
-    ax.plot(ma.index, ma, color='red')
-    ax.set_yscale('log')
-    ax.set_xlim(sc.index.min(), sc.index.max())
-    plt.show()
-
-def viz(length):
-    example_input, example_output = copy(l, length)
-    example_prediction = ntm_fn(example_input)
-    example_ntm = ntm_layer_fn(example_input)
-
-    subplot_shape = (3, 3)
-    ax1 = plt.subplot2grid(subplot_shape, (0, 2))
-    ax1.imshow(example_input[0].T, interpolation='nearest', cmap='bone')
-    ax1.set_title('Input')
-    ax1.get_xaxis().set_visible(False)
-    ax1.get_yaxis().set_visible(False)
-
-    ax2 = plt.subplot2grid(subplot_shape, (1, 2))
-    ax2.imshow(example_output[0].T, interpolation='nearest', cmap='bone')
-    ax2.set_title('Output')
-    ax2.get_xaxis().set_visible(False)
-    ax2.get_yaxis().set_visible(False)
-
-    ax3 = plt.subplot2grid(subplot_shape, (2, 2))
-    ax3.imshow(example_prediction[0].T, interpolation='nearest', cmap='bone')
-    ax3.set_title('Prediction')
-    ax3.get_xaxis().set_visible(False)
-    ax3.get_yaxis().set_visible(False)
-
-    ax4 = plt.subplot2grid(subplot_shape, (0, 1), rowspan=3)
-    ax4.imshow(example_ntm[3][0].T, interpolation='nearest', cmap='bone')
-    ax4.set_title('Read Weights')
-    ax4.get_xaxis().set_visible(False)
-    ax4.plot([length - 0.5, length - 0.5], [0, 127], color='red')
-    ax4.set_xlim([-0.5, 2 * length + 0.5])
-    ax4.set_ylim([-0.5, 127.5])
-
-    ax5 = plt.subplot2grid(subplot_shape, (0, 0), rowspan=3)
-    ax5.imshow(example_ntm[2][0].T, interpolation='nearest', cmap='bone')
-    ax5.set_title('Write Weights')
-    ax5.get_xaxis().set_visible(False)
-    ax5.plot([length - 0.5, length - 0.5], [0, 127], color='red')
-    ax5.set_xlim([-0.5, 2 * length + 0.5])
-    ax5.set_ylim([-0.5, 127.5])
-
-    plt.show()
+markers = [
+    {
+        'location': (lambda params: params['length']),
+        'color': 'red'
+    }
+]
+dashboard = Dashboard(generator=generator, ntm_fn=ntm_fn, ntm_layer_fn=ntm_layer_fn, \
+    memory_shape=memory_shape, markers=markers)
---
+++ utils/generators.py
@@ -0,0 +1,170 @@
+import theano
+import theano.tensor as T
+import numpy as np
+import time
+
+class Task(object):
+
+    def __init__(self, max_iter=None):
+        self.max_iter = max_iter
+        self.num_iter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if (self.max_iter is None) or (self.num_iter < self.max_iter):
+            self.num_iter += 1
+            params = self.sample_params()
+            return self.num_iter, self.sample(**params)
+        else:
+            raise StopIteration()
+
+    def sample_params(self):
+        raise NotImplementedError()
+
+    def sample(self):
+        raise NotImplementedError()
+
+
+class CopyTask(Task):
+
+    def __init__(self, size, max_length, min_length=1, max_iter=None):
+        super(CopyTask, self).__init__(max_iter=max_iter)
+        self.size = size
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def sample_params(self, length=None):
+        if length is None:
+            length = np.random.randint(self.min_length, self.max_length + 1)
+        return {'length': length}
+
+    def sample(self, length):
+        sequence = np.random.binomial(1, 0.5, (length, self.size))
+        example_input = np.zeros((1, 2 * length + 1, self.size + 1), \
+            dtype=theano.config.floatX)
+        example_output = np.zeros((1, 2 * length + 1, self.size + 1), \
+            dtype=theano.config.floatX)
+
+        example_input[0, :length, :self.size] = sequence
+        example_output[0, length + 1:, :self.size] = sequence
+        example_input[0, length, -1] = 1
+
+        return example_input, example_output
+
+
+class RepeatCopyTask(Task):
+
+    def __init__(self, size, max_length, max_repeats=20, min_length=1, \
+        min_repeats=1, max_iter=None):
+        super(RepeatCopyTask, self).__init__(max_iter=max_iter)
+        self.size = size
+        self.min_length = min_length
+        self.max_length = max_length
+        self.min_repeats = min_repeats
+        self.max_repeats = max_repeats
+
+    def sample_params(self, length=None, repeats=None):
+        if length is None:
+            length = np.random.randint(self.min_length, self.max_length + 1)
+        if repeats is None:
+            repeats = np.random.randint(self.min_repeats, self.max_repeats + 1)
+        return {'length': length, 'repeats': repeats}
+
+    def sample(self, length, repeats):
+        sequence = np.random.binomial(1, 0.5, (length, self.size))
+        example_input = np.zeros((1, (repeats + 1) * length + 2, \
+            self.size + 2), dtype=theano.config.floatX)
+        example_output = np.zeros((1, (repeats + 1) * length + 2, \
+            self.size + 2), dtype=theano.config.floatX)
+
+        example_input[0, :length, :self.size] = sequence
+        for j in range(repeats):
+            example_output[0, (j + 1) * length + 2: (j + 2) * length + 2, \
+            :self.size] = sequence
+        example_input[0, length, -2] = repeats / float(self.max_repeats)
+        example_input[0, length + 1, -1] = 1
+
+        return example_input, example_output
+
+
+class AssociativeRecallTask(Task):
+
+    def __init__(self, size, max_item_length, max_num_items, \
+        min_item_length=1, min_num_items=2, max_iter=None):
+        super(AssociativeRecallTask, self).__init__(max_iter=max_iter)
+        self.size = size
+        self.max_item_length = max_item_length
+        self.max_num_items = max_num_items
+        self.min_item_length = min_item_length
+        self.min_num_items = min_num_items
+
+    def sample_params(self, item_length=None, num_items=None):
+        if item_length is None:
+            item_length = np.random.randint(self.min_item_length, \
+                self.max_item_length + 1)
+        if num_items is None:
+            num_items = np.random.randint(self.min_num_items, \
+                self.max_num_items + 1)
+        return {'item_length': item_length, 'num_items': num_items}
+
+    def sample(self, item_length, num_items):
+        def item_slice(j):
+            slice_idx = j * (item_length + 1) + 1
+            return slice(slice_idx, slice_idx + item_length)
+
+        items = np.random.binomial(1, 0.5, (item_length, self.size, num_items))
+        query = np.random.randint(num_items - 1)
+        example_input = np.zeros((1, (item_length + 1) * (num_items + 2), \
+            self.size + 2), dtype=theano.config.floatX)
+        example_output = np.zeros((1, (item_length + 1) * (num_items + 2), \
+            self.size + 2), dtype=theano.config.floatX)
+
+        for j in range(num_items):
+            example_input[0, j * (item_length + 1), -2] = 1
+            example_input[0, item_slice(j), :self.size] = items[:,:,j]
+        example_input[0, num_items * (item_length + 1), -1] = 1
+        example_input[0, item_slice(num_items), :self.size] = items[:,:,query]
+        example_input[0, (num_items + 1) * (item_length + 1), -1] = 1
+        example_output[0, -item_length:, :self.size] = items[:,:,query + 1]
+
+        return example_input, example_output
+
+
+class DynamicNGramsTask(Task):
+
+    def __init__(self, ngrams, max_length, min_length=1, max_iter=None, \
+        table=None):
+        super(DynamicNGramsTask, self).__init__(max_iter=max_iter)
+        self.ngrams = ngrams
+        if table is None:
+            table = self.make_table()
+        self.table = table
+        self.max_length = max(ngrams, max_length)
+        self.min_length = min_length
+        
+    def make_table(self):
+        return np.random.beta(0.5, 0.5, 1 << self.ngrams)
+
+    def sample_params(self, length=None):
+        if length is None:
+            length = np.random.randint(self.min_length, self.max_length + 1)
+        return {'length': length}
+
+    def sample(self, length=None):
+        sequence = np.zeros((1, length + 1, 1), dtype=theano.config.floatX)
+        head = np.random.binomial(1, 0.5, self.ngrams)
+        sequence[0, :self.ngrams, 0] = head
+        index = np.dot(head, 1 << (np.arange(self.ngrams, 0, -1) - 1))
+        mask = (1 << (self.ngrams - 1)) - 1
+
+        for j in range(self.ngrams, length + 1):
+            b = np.random.binomial(1, self.table[index])
+            sequence[0, j, 0] = b
+            index = ((index & mask) << 1) + b
+
+        return sequence[:,:-1], sequence[:,1:]
