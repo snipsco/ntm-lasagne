8618c2a3b498f2e2f7ec1f8df9a5f3cd2350ca63
Tristan Deleu <tristan.deleu@gmail.com>
2016-06-16 16:07:15 +0200

    Rewrite Head classes

--- ntm/heads.py
+++ ntm/heads.py
@@ -1,6 +1,7 @@
 import theano
 import theano.tensor as T
 import numpy as np
+from collections import OrderedDict
 
 from lasagne.layers import Layer, DenseLayer
 from lasagne.theano_extensions import padding
@@ -103,101 +104,45 @@ class Head(Layer):
                  weights_init=init.OneHot(),
                  learn_init=False,
                  **kwargs):
-        super(Head, self).__init__(controller, **kwargs)
 
         self.memory_shape = memory_shape
-        self.basename = kwargs.get('name', 'head')
+        self.name = kwargs.get('name', 'head')
         self.learn_init = learn_init
 
-        if W_hid_to_sign is not None:
-            self.sign = DenseLayer(controller, num_units=self.memory_shape[1],
-                W=W_hid_to_sign, b=b_hid_to_sign, nonlinearity=nonlinearity_sign,
-                name=self.basename + '.sign')
-            self.W_hid_to_sign, self.b_hid_to_sign = self.sign.W, self.sign.b
-        else:
-            self.sign = None
-            self.W_hid_to_sign, self.b_hid_to_sign = None, None
-
-        self.key = DenseLayer(controller, num_units=self.memory_shape[1],
-            W=W_hid_to_key, b=b_hid_to_key, nonlinearity=nonlinearity_key,
-            name=self.basename + '.key')
-        self.W_hid_to_key, self.b_hid_to_key = self.key.W, self.key.b
-        
-        self.beta = DenseLayer(controller, num_units=1,
-            W=W_hid_to_beta, b=b_hid_to_beta, nonlinearity=nonlinearity_beta,
-            name=self.basename + '.beta')
-        self.W_hid_to_beta, self.b_hid_to_beta = self.beta.W, self.beta.b
-
-        self.gate = DenseLayer(controller, num_units=1,
-            W=W_hid_to_gate, b=b_hid_to_gate, nonlinearity=nonlinearity_gate,
-            name=self.basename + '.gate')
-        self.W_hid_to_gate, self.b_hid_to_gate = self.gate.W, self.gate.b
-
-        self.num_shifts = num_shifts
-        self.shift = DenseLayer(controller, num_units=num_shifts,
-            W=W_hid_to_shift, b=b_hid_to_shift, nonlinearity=nonlinearity_shift,
-            name=self.basename + '.shift')
-        self.W_hid_to_shift, self.b_hid_to_shift = self.shift.W, self.shift.b
-
-        self.gamma = DenseLayer(controller, num_units=1,
-            W=W_hid_to_gamma, b=b_hid_to_gamma, nonlinearity=nonlinearity_gamma,
-            name=self.basename + '.gamma')
-        self.W_hid_to_gamma, self.b_hid_to_gamma = self.gamma.W, self.gamma.b
+        # Key
+        self.W_hid_to_key = self.add_param(W_hid_to_key, (1, self.input_shape[1], \
+            self.memory_shape[1]), name=self.name + '.key.W')
+        self.b_hid_to_key = self.add_param(b_hid_to_key, (1, self.memory_shape[1]), \
+            name=self.name + '.key.b', regularizable=False)
+        self.nonlinearity_key = nonlinearity_key
+        # Beta
+        self.W_hid_to_beta = self.add_param(W_hid_to_beta, (1, self.input_shape[1], \
+            1), name=self.name + '.beta.W')
+        self.b_hid_to_beta = self.add_param(b_hid_to_beta, (1, 1), \
+            name=self.name + '.beta.b', regularizable=False)
+        self.nonlinearity_beta = nonlinearity_beta
+        # Gate
+        self.W_hid_to_gate = self.add_param(W_hid_to_gate, (1, self.input_shape[1], \
+            1), name=self.name + '.gate.W')
+        self.b_hid_to_gate = self.add_param(b_hid_to_gate, (1, 1), \
+            name=self.name + '.gate.b', regularizable=False)
+        self.nonlinearity_gate = nonlinearity_gate
+        # Shift
+        self.W_hid_to_shift = self.add_param(W_hid_to_shift, (1, self.input_shape[1], \
+            self.num_shifts), name=self.name + '.shift.W')
+        self.b_hid_to_shift = self.add_param(b_hid_to_shift, (1, self.num_shifts), \
+            name=self.name + '.shift.b', regularizable=False)
+        self.nonlinearity_shift = nonlinearity_shift
+        # Gamma
+        self.W_hid_to_gamma = self.add_param(W_hid_to_gamma, (1, self.input_shape[1], \
+            1), name=self.name + '.gamma.W')
+        self.b_hid_to_gamma = self.add_param(b_hid_to_gamma, (1, 1), \
+            name=self.name + '.gamma.b', regularizable=False)
+        self.nonlinearity_gamma = nonlinearity_gamma
 
         self.weights_init = self.add_param(
             weights_init, (1, self.memory_shape[0]),
             name='weights_init', trainable=learn_init, regularizable=False)
-
-    def get_output_for(self, h_t, w_tm1, M_t, **kwargs):
-        if self.sign is not None:
-            sign_t = self.sign.get_output_for(h_t, **kwargs)
-        else:
-            sign_t = 1.
-        k_t = self.key.get_output_for(h_t, **kwargs)
-        beta_t = self.beta.get_output_for(h_t, **kwargs)
-        g_t = self.gate.get_output_for(h_t, **kwargs)
-        s_t = self.shift.get_output_for(h_t, **kwargs)
-        gamma_t = self.gamma.get_output_for(h_t, **kwargs)
-
-        # Content Adressing (3.3.1)
-        beta_t = T.addbroadcast(beta_t, 1)
-        betaK = beta_t * similarities.cosine_similarity(sign_t * k_t, M_t)
-        w_c = lasagne.nonlinearities.softmax(betaK)
-
-        # Interpolation (3.3.2)
-        g_t = T.addbroadcast(g_t, 1)
-        w_g = g_t * w_c + (1. - g_t) * w_tm1
-
-        # Convolutional Shift (3.3.2)
-        w_g_padded = w_g.dimshuffle(0, 'x', 'x', 1)
-        conv_filter = s_t.dimshuffle(0, 'x', 'x', 1)
-        pad = (self.num_shifts // 2, (self.num_shifts - 1) // 2)
-        w_g_padded = padding.pad(w_g_padded, [pad], batch_ndim=3)
-        convolution = T.nnet.conv2d(w_g_padded, conv_filter,
-            input_shape=(self.input_shape[0], 1, 1, self.memory_shape[0] + pad[0] + pad[1]),
-            filter_shape=(self.input_shape[0], 1, 1, self.num_shifts),
-            subsample=(1, 1),
-            border_mode='valid')
-        w_tilde = convolution[:, 0, 0, :]
-
-        # Sharpening (3.3.2)
-        gamma_t = T.addbroadcast(gamma_t, 1)
-        w = T.pow(w_tilde + 1e-6, gamma_t)
-        w /= T.sum(w)
-
-        return w
-
-    def get_params(self, **tags):
-        params = super(Head, self).get_params(**tags)
-        if self.sign is not None:
-            params += self.sign.get_params(**tags)
-        params += self.key.get_params(**tags)
-        params += self.beta.get_params(**tags)
-        params += self.gate.get_params(**tags)
-        params += self.shift.get_params(**tags)
-        params += self.gamma.get_params(**tags)
-
-        return params
 
 
 class WriteHead(Head):
@@ -301,34 +246,18 @@ class WriteHead(Head):
             W_hid_to_shift=W_hid_to_shift, b_hid_to_shift=b_hid_to_shift, nonlinearity_shift=nonlinearity_shift,
             W_hid_to_gamma=W_hid_to_gamma, b_hid_to_gamma=b_hid_to_gamma, nonlinearity_gamma=nonlinearity_gamma,
             weights_init=weights_init, learn_init=learn_init, **kwargs)
-    
-        self.erase = DenseLayer(controller, num_units=self.memory_shape[1],
-            W=W_hid_to_erase, b=b_hid_to_erase, nonlinearity=nonlinearity_erase,
-            name=self.basename + '.erase')
-        self.W_hid_to_erase, self.b_hid_to_erase = self.erase.W, self.erase.b
-
-        self.add = DenseLayer(controller, num_units=self.memory_shape[1],
-            W=W_hid_to_add, b=b_hid_to_add, nonlinearity=nonlinearity_add,
-            name=self.basename + '.add')
-        self.W_hid_to_add, self.b_hid_to_add = self.add.W, self.add.b
-
-        if W_hid_to_sign_add is not None:
-            self.sign_add = DenseLayer(controller, num_units=self.memory_shape[1],
-                W=W_hid_to_sign_add, b=b_hid_to_sign_add, nonlinearity=nonlinearity_sign_add,
-                name=self.basename + '.sign_add')
-            self.W_hid_to_sign_add, self.b_hid_to_sign_add = self.sign_add.W, self.sign_add.b
-        else:
-            self.sign_add = None
-            self.W_hid_to_sign_add, self.b_hid_to_sign_add = None, None
-
-    def get_params(self, **tags):
-        params = super(WriteHead, self).get_params(**tags)
-        params += self.erase.get_params(**tags)
-        params += self.add.get_params(**tags)
-        if self.sign_add is not None:
-            params += self.sign_add.get_params(**tags)
-
-        return params
+        # Erase
+        self.W_hid_to_erase = self.add_param(W_hid_to_erase, (1, self.input_shape[1], \
+            self.memory_shape[1]), name=self.name + '.erase.W')
+        self.b_hid_to_erase = self.add_param(b_hid_to_erase, (1, self.memory_shape[1]), \
+            name=self.name + '.erase.b', regularizable=False)
+        self.nonlinearity_erase = nonlinearity_erase
+        # Add
+        self.W_hid_to_add = self.add_param(W_hid_to_add, (1, self.input_shape[1], \
+            1), name=self.name + '.add.W')
+        self.b_hid_to_add = self.add_param(b_hid_to_add, (1, 1), \
+            name=self.name + '.add.b', regularizable=False)
+        self.nonlinearity_add = nonlinearity_add
 
 
 class ReadHead(Head):
@@ -409,7 +338,6 @@ class ReadHead(Head):
 class HeadCollection(object):
     """docstring for HeadCollection"""
     def __init__(self, heads):
-        super(HeadCollection, self).__init__()
         self.heads = heads
         # Key
         self.W_hid_to_key = T.concatenate([head.W_hid_to_key for head in self.heads], axis=0)
@@ -431,6 +359,8 @@ class HeadCollection(object):
         self.W_hid_to_gamma = T.concatenate([head.W_hid_to_gamma for head in self.heads], axis=0)
         self.b_hid_to_gamma = T.concatenate([head.b_hid_to_gamma for head in self.heads], axis=0)
         self.nonlinearity_gamma = self.heads[0].nonlinearity_gamma
+        # Initialization
+        self.weights_init = T.concatenate([head.weights_init for head in self.heads], axis=0)
 
     def get_params(self, **tags):
         params = []
@@ -458,6 +388,15 @@ class HeadCollection(object):
 
         # Convolutional Shift (3.3.2)
         # TODO
+        # w_g_padded = w_g.dimshuffle(0, 'x', 'x', 1)
+        # conv_filter = s_t.dimshuffle(0, 'x', 'x', 1)
+        # pad = (self.num_shifts // 2, (self.num_shifts - 1) // 2)
+        # w_g_padded = padding.pad(w_g_padded, [pad], batch_ndim=3)
+        # convolution = T.nnet.conv2d(w_g_padded, conv_filter,
+        #     input_shape=(self.input_shape[0], 1, 1, self.memory_shape[0] + pad[0] + pad[1]),
+        #     filter_shape=(self.input_shape[0], 1, 1, self.num_shifts),
+        #     subsample=(1, 1),
+        #     border_mode='valid')
         w_tilde = w_g
 
         # Sharpening (3.3.2)
